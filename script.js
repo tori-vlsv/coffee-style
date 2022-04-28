701df469e8f63d9e8264233e60e918a09795c854
tori-vlsv <victoriaavlasova@gmail.com>
2022-04-28 21:33:22 +0300

    add responsive and fixes

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ $(window).on('scroll', function() {
         header.removeClass('active');
     }
 });
+
+function toggleMobileMenu() {
+    let burger = $('.burger__button');
+    let menu = $('nav');
+    let close = $('.menu__close');
+    burger.on('click', function() {
+        menu.addClass('active');
+    });
+    close.on('click', function() {
+        menu.removeClass('active');
+    });
+}
+toggleMobileMenu();
